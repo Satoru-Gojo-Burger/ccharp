de0ba3aeeea5fa09c75d1f3b613875ce6b7e2e89
Satoru-Gojo-Burger <Grah228@yandex.ru>
2024-10-18 18:07:47 +0300

    2

--- ConsoleApp_xlab/Program.cs
+++ ConsoleApp_xlab/Program.cs
@@ -53,6 +53,13 @@
         Console.WriteLine($"list - {string.Join(", ", list)}");
         list.Remove(3);
         Console.WriteLine($"list - {string.Join(", ", list)}");
+
+
+        MyList myList = new MyList();
+        myList.Add(1);
+        myList.Add(2);
+        myList.Add(3);
+        myList.Add(4);
     }
 
     public static void func(ref int o)
